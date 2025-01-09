d58a9c7a0f27a5760c06d11dd67c6136bea41b6c
t-toshiro <tsrbasuke@icloud.com>
2025-01-09 23:09:05 +0900

    アイコン追加

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -17,8 +17,46 @@ const HomePage = () => {
         津軽寿史郎です。現在、私は筑波大学の一年生で大学での勉強と両立しながらプログラミングをしています。プログラミングは2024年の夏から始めて、Python,HTML/CSS,Reactなどをこれまで学習してきました。
       </p>
 
-      
-      
+      <section class="page-section" id="services">
+        <div className="service">
+          <div class="text-center">
+            <h2 class="section-heading text-uppercase"></h2>
+            <h3 class="section-subheading text-muted mb-5">
+              
+            </h3>
+          </div>
+          <div class="row text-center">
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+              <i class="fa-solid fa-fire fa-2xl"></i>
+              </span>
+              <h4 class="my-3">今ハマっていること</h4>
+              <p class="text-muted">
+                謎解きにハマっており、友人と一緒に色々な謎解き公演に参加しています。
+              </p>
+            </div>
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+           
+               <i class="fa-solid fa-basketball fa-2xl"></i>
+              </span>
+              <h4 class="my-3">趣味</h4>
+              <p class="text-muted">
+                小学校の時にバスケットボールをはじめ、中学、高校もバスケ部に所属していました。
+              </p>
+            </div>
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+              <i class="fa-solid fa-bullseye fa-2xl"></i>
+              </span>
+              <h4 class="my-3">目標</h4>
+              <p class="text-muted">
+                インターンの中で実務経験を積み、より優れたエンジニアになることが目標です。
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <section id="skill">
         <div class="text-center">
