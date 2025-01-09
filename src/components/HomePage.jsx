4e67d79d96bdc82fd4afe368e5af4df20b0f0d17
t-toshiro <tsrbasuke@icloud.com>
2025-01-09 22:13:22 +0900

    完成２

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import reactImage from "../Images/react.png";
 import jsImage from "../Images/js.png";
-import profileImage from "../Images/sibainu.jpg";
+import profileImage from "../Images/IMG_2658.jpg";
 import pythonImage from "../Images/python_18894.png"
+
 
 
 const HomePage = () => {
@@ -16,54 +17,8 @@ const HomePage = () => {
         津軽寿史郎です。現在、私は筑波大学の一年生で大学での勉強と両立しながらプログラミングをしています。プログラミングは2024年の夏から始めて、Python,HTML/CSS,Reactなどをこれまで学習してきました。
       </p>
 
-      <section class="page-section" id="services">
-        <div className="service">
-          <div class="text-center">
-            <h2 class="section-heading text-uppercase"></h2>
-            <h3 class="section-subheading text-muted mb-5">
-              
-            </h3>
-          </div>
-          <div class="row text-center">
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">ECサイト</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">レスポンシブサイト</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">ウェブセキュリティ</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      
+      
 
       <section id="skill">
         <div class="text-center">
